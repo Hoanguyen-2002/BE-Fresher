cc7532b99015993001731b23a7bf9ba00a3c875a
Nguyễn Việt Hòa <84431330+Hoanguyen-2002@users.noreply.github.com>
2024-11-20 11:21:59 +0700

    update repository

--- src/main/java/com/lg/fresher/lgcommerce/repository/review/ReviewImageRepository.java
+++ src/main/java/com/lg/fresher/lgcommerce/repository/review/ReviewImageRepository.java
@@ -2,6 +2,8 @@ package com.lg.fresher.lgcommerce.repository.review;
 
 import com.lg.fresher.lgcommerce.entity.review.ReviewImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReviewImageRepository  extends JpaRepository<ReviewImage,String> {
 }
